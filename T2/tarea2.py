835d64bc6e217bded480625ba3b7c0f652739cf8
Alberto Garcia FM <albertogarciafigueroam@gmail.com>
2022-02-18 21:48:05 -0600

    done

--- T2/tarea2.py
+++ T2/tarea2.py
@@ -1,118 +1,139 @@
+def codificar_letra(letra):
+    if letra == 'A':
+        return '90'
+    elif letra == 'B':
+        return '91'
+    elif letra == 'C':
+        return '92'
+    elif letra == 'D':
+        return '93'
+    elif letra == 'E':
+        return '94'
+    elif letra == 'F':
+        return '80'
+    elif letra == 'G':
+        return '81'
+    elif letra == 'H':
+        return '82'
+    elif letra == 'I' or letra == 'J':
+        return '83'
+    elif letra == 'K':
+        return '84'
+    elif letra == 'L':
+        return '70'
+    elif letra == 'M':
+        return '71'
+    elif letra == 'N':
+        return '72'
+    elif letra == 'O':
+        return '73'
+    elif letra == 'P':
+        return '74'
+    elif letra == 'Q':
+        return '60'
+    elif letra == 'R':
+        return '61'
+    elif letra == 'S':
+        return '62'
+    elif letra == 'T':
+        return '63'
+    elif letra == 'U':
+        return '64'
+    elif letra == 'V':
+        return '50'
+    elif letra == 'W':
+        return '51'
+    elif letra == 'X':
+        return '52'
+    elif letra == 'Y':
+        return '53'
+    elif letra == 'Z':
+        return '54'
+
+def decodificar_letra(par_num):
+    if par_num == '90':
+        return 'A'
+    elif par_num == '91':
+        return 'B'
+    elif par_num == '92':
+        return 'C'
+    elif par_num == '93':
+        return 'D'
+    elif par_num == '94':
+        return 'E'
+    elif par_num == '80':
+        return 'F'
+    elif par_num == '81':
+        return 'G'
+    elif par_num == '82':
+        return 'H'
+    elif par_num == '83':
+        return 'I'
+    elif par_num == '84':
+        return 'K'
+    elif par_num == '70':
+        return 'L'
+    elif par_num == '71':
+        return 'M'
+    elif par_num == '72':
+        return 'N'
+    elif par_num == '73':
+        return 'O'
+    elif par_num == '74':
+        return 'P'
+    elif par_num == '60':
+        return 'Q'
+    elif par_num == '61':
+        return 'R'
+    elif par_num == '62':
+        return 'S'
+    elif par_num == '63':
+        return 'T'
+    elif par_num == '64':
+        return 'U'
+    elif par_num == '50':
+        return 'V'
+    elif par_num == '51':
+        return 'W'
+    elif par_num == '52':
+        return 'X'
+    elif par_num == '53':
+        return 'Y'
+    elif par_num == '54':
+        return 'Z'
+    else:
+        return None
+
+def codificar_palabra(palabra):
+    palabra_codificada = ''
+    for i in range(len(palabra)):
+        if(codificar_letra(palabra[i]) != None):
+            palabra_codificada += codificar_letra(palabra[i])
+        else:
+            print('LA PALABRA CONTIENE CARACTERES ILEGALES')
+            return None
+    print("LA PALABRA CODIFICADA ES: "+palabra_codificada)
+
+def decodificar_palabra(code):
+    if(not(code.isnumeric() and len(code)%2 == 0)):
+        print("INGRESE UNA PALABRA CODIFICADA VÁLIDA")
+        return None
+    palabra_decodificada = ''
+    for i in range(len(code)):
+        if(i%2 == 0):
+            if(decodificar_letra(code[i:i+2]) == None):
+                print("LA PALABRA NO SE PUDO DECODIFICAR")
+                return None
+            else:
+                palabra_decodificada += str(decodificar_letra(code[i:i+2]))
+        else:
+            pass
+    print("LA PALABRA DECODIFICADA ES: "+palabra_decodificada)
+
 def normalizar_cadena(palabra):
     palabra = palabra.upper()
     palabra = palabra.replace(" ","")
     return palabra
-
-def codificar_letra(letra):
-    if letra == 'A':
-        return '09'
-    elif letra == 'B':
-        return '19'
-    elif letra == 'C':
-        return '29'
-    elif letra == 'D':
-        return '39'
-    elif letra == 'E':
-        return '49'
-    elif letra == 'F':
-        return '08'
-    elif letra == 'G':
-        return '18'
-    elif letra == 'H':
-        return '28'
-    elif letra == 'I' or letra == 'J':
-        return '38'
-    elif letra == 'K':
-        return '48'
-    elif letra == 'L':
-        return '07'
-    elif letra == 'M':
-        return '17'
-    elif letra == 'N':
-        return '27'
-    elif letra == 'O':
-        return '37'
-    elif letra == 'P':
-        return '47'
-    elif letra == 'Q':
-        return '06'
-    elif letra == 'R':
-        return '16'
-    elif letra == 'S':
-        return '26'
-    elif letra == 'T':
-        return '36'
-    elif letra == 'U':
-        return '46'
-    elif letra == 'V':
-        return '05'
-    elif letra == 'W':
-        return '15'
-    elif letra == 'X':
-        return '25'
-    elif letra == 'Y':
-        return '35'
-    elif letra == 'Z':
-        return '45'
-
-def decodificar_letra():
-    if letra == '09':
-        return 'A'
-    elif letra == '19':
-        return 'B'
-    elif letra == '29':
-        return 'C'
-    elif letra == '39':
-        return 'D'
-    elif letra == '49':
-        return 'E'
-    elif letra == '08':
-        return 'F'
-    elif letra == '18':
-        return 'G'
-    elif letra == '28':
-        return 'H'
-    elif letra == '38':
-        return 'I'
-    elif letra == '48':
-        return 'K'
-    elif letra == '07':
-        return 'L'
-    elif letra == '17':
-        return 'M'
-    elif letra == '27':
-        return 'N'
-    elif letra == '37':
-        return 'O'
-    elif letra == '47':
-        return 'P'
-    elif letra == '06':
-        return 'Q'
-    elif letra == '16':
-        return 'R'
-    elif letra == '26':
-        return 'S'
-    elif letra == '36':
-        return 'T'
-    elif letra == '46':
-        return 'U'
-    elif letra == '05':
-        return 'V'
-    elif letra == '15':
-        return 'W'
-    elif letra == '25':
-        return 'X'
-    elif letra == '35':
-        return 'Y'
-    elif letra == '45':
-        return 'Z'
-
-def codificar_palabra(palabra):
-    palabra_codificada = ''
-    palabra_limpia = normalizar_cadena(palabra)
-    for i in range(len(palabra)):
-        palabra_codificada += codificar_letra(palabra[i])
-    print("LA PALABRA CODIFICADA ES: "+palabra_codificada)
 
 
 def main():
@@ -120,10 +141,14 @@ def main():
     print("ESCOGA UNA OPCION")
     print("1.-CODIFICAR MENSAJE")
     print("2.-DECODIFICAR MENSAJE")
-    desicion = int(input('->'))
-    if desicion == 1:
-        
-    elif desicion == 2:
-
+    decision = int(input('->'))
+    if decision == 1:
+        word =  input('INGRESE LA FRASE A CODIFICAR\n-->')
+        codificar_palabra(normalizar_cadena(word))
+    elif decision == 2:
+        code = input('INGRESE LA FRASE A DECODIFICAR\n-->')
+        decodificar_palabra(code)    
     else:
         print("INGRESE UN VALOR CORRECTO") 
+
+main()
