ead9fd09cd2db06100f0602538c8d08b5e0d3e14
albertogfm <albertogarciafigueroam@gmail.com>
2022-03-02 00:51:02 -0600

    docs and code done

--- TAREAS/T4/tarea4.py
+++ TAREAS/T4/tarea4.py
@@ -1,46 +1,49 @@
 import string, random
 
-
-
 def codificar_palabra(frase,clave):
-    '''Takes the word to be coded and prints it. Returns nothing'''
+    '''Takes the phrase to be coded, transforms it to the coded table and returns the frase coded. Returns coded phrase'''
     frase= normalizar_cadena(frase)
+    if(validar_key(frase,clave) == None):
+        return None
     tabla_codificacion = []    
     frase_codificada = ''
-    for i in range(len(frase)//clave):
+    valores_completos = len(frase)//clave
+    extra_val = len(frase)%clave
+    for i in range(valores_completos):
         if i==0:
             tabla_codificacion.append(frase[0:clave])
         else:
             tabla_codificacion.append(frase[i*clave:(i*clave)+clave])
-    if(len(frase)%clave != 0):
-        frase_con_digitos= frase[(len(frase)//clave)*clave:((len(frase)//clave)*clave)+(len(frase)%clave)]
-        for i in range(clave-len(frase)%clave): 
+    if(extra_val != 0):
+        frase_con_digitos= frase[(valores_completos)*clave:((valores_completos)*clave)+(extra_val)]
+        for i in range(clave-extra_val): 
             frase_con_digitos+= random.choice(string.ascii_letters).upper()
         tabla_codificacion.append(frase_con_digitos)
-    print(tabla_codificacion)
+    #print(tabla_codificacion)
     for j in range(clave):
         for i in range(len(tabla_codificacion)):
             frase_codificada += tabla_codificacion[i][j]
-    print(frase_codificada)
+    return(frase_codificada)
    
     
 def decodificar_palabra(frase_codif,clave):
-    '''Takes the coded word and prints it. Returns None if the coded word is invalid'''
+    '''Takes the coded phrase, decodes the table and retruns it decoded. Returns None if the key is invalid'''
     tabla_decod = []
     frase_decodificada=''
+    if(validar_key(frase_codif,clave) == None):
+        return None
     factor_codif = len(frase_codif)//clave
     for i in range(clave):
         if i==0:
             tabla_decod.append(frase_codif[0:factor_codif])
         else:
             tabla_decod.append(frase_codif[i*factor_codif:(i*factor_codif)+factor_codif])
-
-    print(tabla_decod)
+    #print(tabla_decod)
     for j in range(factor_codif):
         for i in range(len(tabla_decod)):
             frase_decodificada += tabla_decod[i][j]
-    print(frase_decodificada)
-    #return frase_decodificada
+    #print(frase_decodificada)
+    return frase_decodificada
 
 def normalizar_cadena(frase):
     '''Takeas a sentence. returns the sentence without spaces and all in uppercase'''
@@ -48,11 +51,17 @@ def normalizar_cadena(frase):
     frase = frase.replace(" ","")
     return frase
 
+def validar_key(frase,key):
+    """Validates if the key is above one and is not under the length of the phrase to be coded // uncoded"""
+    if(len(frase) <= key or len(frase) == 0 or key <=1):
+        return None
+    else:
+        return True
 
 def main():
     '''Main function that starts the program '''
     while(True):
-        print("====================================")
+        print("==================================================   ")
         print("BIENVENIDO AL SISTEMA DECODIFICADOR AGENTE SECRETO")
         print("ESCOGA UNA OPCION")
         print("1.-CODIFICAR MENSAJE")
@@ -64,21 +73,21 @@ def main():
             key = int(input('INGRESE LA CLAVE PARA CODIFICAR\n-->'))
             codificado = codificar_palabra(word,key)  
             if codificado != None:
-                print("LA PALABRA CODIFICADA ES: "+codificado)
+                print("LA FRASE CODIFICADA ES: "+codificado)
             else:
-                print('LA PALABRA CONTIENE CARACTERES ILEGALES')
+                print('LA FRASE NO SE PUDO CODIFICAR, VERIFIQUE SU CLAVE')
         elif decision == 2:
             code = input('INGRESE LA FRASE A DECODIFICAR\n-->')
             key = int(input('INGRESE LA CLAVE PARA DECODIFICAR\n-->'))
             decodificado = decodificar_palabra(code,key)  
             if decodificado != None:
-                print("LA PALABRA DECODIFICADA ES: "+decodificado)
+                print("LA FRASE DECODIFICADA ES: "+decodificado)
             else:
-                print("INGRESE UNA PALABRA CODIFICADA VÁLIDA")
+                print("LA FRASE NO SE PUDO CODIFICAR, VERIFIQUE SU CLAVE")
         elif decision == 3:
             return
         else:
             print("INGRESE UN VALOR CORRECTO") 
-        print("====================================")
+        print("==================================================")
 
 main()
--- TAREAS/T4/test_tarea2.py
+++ TAREAS/T4/test_tarea2.py
@@ -4,13 +4,13 @@ import tarea4
 class TestCodificarPalabra(unittest.TestCase):
     def test_codificar(self):
         #Test when phrase is hola mundo
-        self.assertEqual(tarea4.codificar_palabra('hola mundo'),'827370907164729373')
+        self.assertEqual(tarea4.codificar_palabra('la criptografia es romantica',4)[:10],'LIGIRNAAPR')
         self.assertEqual(tarea4.codificar_palabra('HOLA MUNDO'),'827370907164729373')
         self.assertEqual(tarea4.codificar_palabra('hOla   MunDo'),'827370907164729373')
         self.assertEqual(tarea4.codificar_palabra('hOla   MuñDo'),None)
     
     def test_decodificar(self):
-        self.assertEqual(tarea4.decodificar_palabra('827370907164729373'),'HOLAMUNDO')
+        self.assertEqual(tarea4.decodificar_palabra('LIGIRNAAPRAOTZCTAEMIIROFSACG'),'LACRIPTOGRAFIAESROMANTICAZIG')
         self.assertEqual(tarea4.decodificar_palabra('82737097164729373'),None)
     
     def test_normalizar(self):
