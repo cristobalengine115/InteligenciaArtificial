489c84060a670b42adc6f74cda7a0593d42dfc22
albertogfm <albertogarciafigueroam@gmail.com>
2022-03-23 23:22:03 -0600

    start of t6

---
+++ TAREAS/T6/tarea6.py
@@ -0,0 +1,11 @@
+def evaluar(fp):
+    dicOUna = {'!': 'not '}
+    dicOBin = {'|': ' or ','&':' and ','=>':' '}
+    ops , val = [] , []
+    data = fp.split()
+    pass
+
+def simplificar(p):
+    pass
+def base(p):
+    pass
