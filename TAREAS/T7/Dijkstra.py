ba8831c535991a1e5f561f7a82d0eddb3b28260f
Edgar Garcia <cristobalstressed115@gmail.com>
2022-04-23 16:29:55 -0500

    A estrella

--- TAREAS/T7/Dijkstra.py
+++ TAREAS/T7/Dijkstra.py
@@ -1,5 +1,8 @@
+import string
 class Nodo:
+	'''Clase Nodo que permite almacenar la infromación independientemente'''
 	def __init__(self,nombre,dist_tentativa,vecinos,euristica):
+		'''Constructor'''
 		self.nombre = nombre
 		self.anterior = None
 		self.dist_tentativa = dist_tentativa
@@ -8,6 +11,7 @@ class Nodo:
 		self.orden = []
 		self.visitado = False
 	def impri_grafo_info(self):
+		'''Imprime la información del grafo y si detecta una eurisitica de -1 se desprecia'''
 		print('Nodo: ' +str(self.nombre) + ' -Distancia tentativa: '+ str('infinito' if self.dist_tentativa == 1000 else self.dist_tentativa) +' -Vecinos: '+ str(self.vecinos) +' -Anterior: '+ str(self.anterior), end = '')
 		if self.euristica != -1:
 			print(' -Euristica: '+str(self.euristica))
@@ -15,30 +19,35 @@ class Nodo:
 			print('')
 
 class Grafo:
-
+	'''Clase grafo que almacena los nodos'''
 	def __init__(self, nodo_inicial):
+		'''Constructor'''
 		self.nodos = {}
 		self.nodo_no_visitados = []
 		self.nodo_inicial = nodo_inicial
 
 	def getNodo(self,nombreNodo):
+		'''Devuelve un nodo a partir de un nombre'''
 		for i in range (0,len(self.nodos)):
 			if (i.nombre == self.nombreNodo):
 				return i
 
 	def impriGrafo(self):
+		"""Imprimer la información de todos los nodos del grafo"""
 		for i in self.nodos:
 			self.nodos[i].impri_grafo_info()
 		print("==========================================================================================================")
 
 	def createGrafo(self,grafo,banderaEuristica = None):
+		'''Crea un grafo a partir de una cadena, si se incluye un boolen al final se guardaran laseuristicas '''
 		nodes = grafo.split(';')
 		for i in nodes:
 			vecinos = {}
 			vecinos_n = i.split(',')
 			tamanio = len(vecinos_n) -1
 			for j in range(tamanio):
-				vecinos[vecinos_n[j+1][0]] = vecinos_n[j+1][1:]  ####que
+				vecinos[vecinos_n[j+1][0]] = vecinos_n[j+1][1:]  
+				#Euristicas
 				if banderaEuristica is None:
 					if (len(self.nodos) == 0):
 						nodo = Nodo(vecinos_n[0],0,vecinos,-1)
@@ -55,6 +64,7 @@ class Grafo:
 	
 
 	def minimo(self, nodos):
+		'''Método que determina el menor costo de la distancia entre el nodo origen y final'''
 		if len(nodos) > 0:
 			minus = self.nodos[nodos[0]].dist_tentativa
 			vector = nodos[0]
@@ -81,6 +91,7 @@ class Grafo:
 
 		route.reverse()
 		print('La distancia mas corta es: '+ str(route))
+		return route
 
 	def dijkstra(self,destino):
 		actual = self.nodos[self.nodo_inicial].nombre
@@ -88,8 +99,8 @@ class Grafo:
 			self.nodo_no_visitados.append(v)
 		while len(self.nodo_no_visitados) > 0:
 			if actual == destino :
-				self.rutaD(destino)
-				return True
+				return self.rutaD(destino)
+				#return True
 			for i in self.nodos[actual].vecinos:
 				if self.nodos[i[0]].visitado == False:
 					if self.nodos[actual].dist_tentativa + int(self.nodos[actual].vecinos[i]) < self.nodos[i].dist_tentativa:
@@ -98,9 +109,16 @@ class Grafo:
 			self.nodos[actual].visitado = True
 			self.nodo_no_visitados.remove(actual)
 			actual = self.minimo(self.nodo_no_visitados)
-	
-	# def a_star(self):
-		
+
+	def a_star(self,nodo_final):	
+		self.nodo_no_visitados().append(self.nodos(self.nodo_inicial))
+		actual = self.nodo_no_visitados[0]
+		while(len(self.nodo_no_visitados() != 0 )):
+			if(actual.nombre == self.nodos(nodo_final).nombre ):
+				#Metodo
+
+				pass
+			
 		
 	
 
